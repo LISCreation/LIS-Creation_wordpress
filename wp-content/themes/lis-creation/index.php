8dbaab9ec8fb551851d1ef2e5231e925e94a3824
LIS Creation <lis.lightinsky@gmail.com>
2018-10-19 00:10:32 +0300

    Add parts for pages

--- wp-content/themes/lis-creation/index.php
+++ wp-content/themes/lis-creation/index.php
@@ -255,102 +255,13 @@
                             Хотите продающий сайт? Теперь вы знаете к кому
                             обращаться!</p>
                     </div>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse team-button">Наши кейсы</div></div></a>
+                    <a class="works-link" href="<?=get_permalink(9);?>"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse team-button">Наши кейсы</div></div></a>
                 </div>
 
             </div>
         </div>
     </section>
-    <section class="services" id="services">
-        <div class="container-fluid p-0">
-            <div class="section-info">
-                <h2 class="section-header">наши услуги</h2>
-                <p class="section-description">За 4 года более восьмидесяти клиентов стали довольными
-                    обладателями собственных страниц в интернете. Наша задача,
-                    чтобы посетители страницы стали вашими покупателями.</p>
-            </div>
-            <div class="services-list owl-carousel">
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Landing Page</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/landing.png" alt="Landing Page by LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>21 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">Мобильная версия сайта</li>
-                        <li class="services-list_item-apps_thing">Уникальный дизайн</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация</li>
-                        <li class="services-list_item-apps_thing">Размещение на хостинге</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Интернет-магазин</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/ecommerce-shop.png" alt="Landing Page LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>32 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">Мобильная версия сайта</li>
-                        <li class="services-list_item-apps_thing">Уникальный дизайн</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация</li>
-                        <li class="services-list_item-apps_thing">CMS на выбор **</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Блог / Информационный сайт</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="Landing Page LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>27 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">CMS для работы с контентом</li>
-                        <li class="services-list_item-apps_thing">Многостраничный сайт</li>
-                        <li class="services-list_item-apps_thing">Мобильная оптимизация</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация (x2)</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Корпоративный сайт</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="Landing Page LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>29 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">Мобильная версия сайта</li>
-                        <li class="services-list_item-apps_thing">Система управления</li>
-                        <li class="services-list_item-apps_thing">Более 10 страниц</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Промо-сайт</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="Landing Page LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>21 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">Мобильная версия сайта</li>
-                        <li class="services-list_item-apps_thing">Ориентация на праздник</li>
-                        <li class="services-list_item-apps_thing">Fast-Маркетинг</li>
-                        <li class="services-list_item-apps_thing">Контекстная реклама</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Сайт-визитка</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/visit.png" alt="Landing Page LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>24 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">CMS для работы с контентом</li>
-                        <li class="services-list_item-apps_thing">Уникальный дизайн</li>
-                        <li class="services-list_item-apps_thing">Многостраничный сайт</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-            </div>
-        </div>
-    </section>
+    <?php get_template_part('template-parts/section_service', 'main');?>
     <section class="why" id="why">
         <div class="container">
             <div class="section-info">
@@ -386,33 +297,5 @@
             </div>
         </div>
     </section>
-    <section class="callback" id="callback">
-        <div class="container">
-            <div class="section-info">
-                <h2 class="section-header">свяжитесь с нами</h2>
-                <p class="section-description">Заполните форму и мы с Вами свяжемся
-                    в ближайшее время!</p>
-            </div>
-            <form action="javascript:void(null)" id="callback_main" method="post" class="callback-form">
-                <div class="callback-form_inner">
-                    <div class="inp_wrapper">
-                        <input type="text" name="name" placeholder="Ваше имя" class="inp callback-form-input">
-                        <span class="inp_starlet">*</span>
-                    </div>
-                    <div class="inp_wrapper">
-                        <input type="tel" name="phone" placeholder="Введите номер" class="inp callback-form-input phone_inp">
-                        <span class="inp_starlet">*</span>
-                    </div>
-                    <div class="inp_wrapper">
-                        <input type="email" name="email" placeholder="Ваш E-mail" class="inp callback-form-input">
-                        <span class="inp_starlet">*</span>
-                    </div>
-                    <div class="callback-form-button_wrapper">
-                        <div class="btn_roundness"><input type="submit" value="Оставить заявку" class="btn callback-form-button"></div>
-                    </div>
-                </div>
-                <div id="callback_main_result"></div>
-            </form>
-        </div>
-    </section>
+<?php get_template_part('template-parts/secion_callback', 'main');?>
 <?php wp_footer().get_footer();?>
---
+++ wp-content/themes/lis-creation/template-parts/section_service-main.php
@@ -0,0 +1,90 @@
+<section class="services" id="services">
+	<div class="container-fluid p-0">
+		<div class="section-info">
+			<h2 class="section-header">наши услуги</h2>
+			<p class="section-description">За 4 года более восьмидесяти клиентов стали довольными
+				обладателями собственных страниц в интернете. Наша задача,
+				чтобы посетители страницы стали вашими покупателями.</p>
+		</div>
+		<div class="services-list owl-carousel">
+			<article class="services-list_item">
+				<h3 class="services-list_item-title">Landing Page</h3>
+				<img src="<?=get_template_directory_uri();?>/assets/img/services/landing.png" alt="Landing Page by LIS" class="services-list_item-example">
+				<p class="services-list_item-price">от <span>21 990</span> р.</p>
+				<ul class="services-list_item-apps">
+					<li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
+					<li class="services-list_item-apps_thing">Мобильная версия сайта</li>
+					<li class="services-list_item-apps_thing">Уникальный дизайн</li>
+					<li class="services-list_item-apps_thing">SEO оптимизация</li>
+					<li class="services-list_item-apps_thing">Размещение на хостинге</li>
+				</ul>
+				<a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
+			</article>
+			<article class="services-list_item">
+				<h3 class="services-list_item-title">Интернет-магазин</h3>
+				<img src="<?=get_template_directory_uri();?>/assets/img/services/ecommerce-shop.png" alt="Landing Page LIS" class="services-list_item-example">
+				<p class="services-list_item-price">от <span>32 990</span> р.</p>
+				<ul class="services-list_item-apps">
+					<li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
+					<li class="services-list_item-apps_thing">Мобильная версия сайта</li>
+					<li class="services-list_item-apps_thing">Уникальный дизайн</li>
+					<li class="services-list_item-apps_thing">SEO оптимизация</li>
+					<li class="services-list_item-apps_thing">CMS на выбор **</li>
+				</ul>
+				<a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
+			</article>
+			<article class="services-list_item">
+				<h3 class="services-list_item-title">Блог / Информационный сайт</h3>
+				<img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="Landing Page LIS" class="services-list_item-example">
+				<p class="services-list_item-price">от <span>27 990</span> р.</p>
+				<ul class="services-list_item-apps">
+					<li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
+					<li class="services-list_item-apps_thing">CMS для работы с контентом</li>
+					<li class="services-list_item-apps_thing">Многостраничный сайт</li>
+					<li class="services-list_item-apps_thing">Мобильная оптимизация</li>
+					<li class="services-list_item-apps_thing">SEO оптимизация (x2)</li>
+				</ul>
+				<a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
+			</article>
+			<article class="services-list_item">
+				<h3 class="services-list_item-title">Корпоративный сайт</h3>
+				<img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="Landing Page LIS" class="services-list_item-example">
+				<p class="services-list_item-price">от <span>29 990</span> р.</p>
+				<ul class="services-list_item-apps">
+					<li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
+					<li class="services-list_item-apps_thing">Мобильная версия сайта</li>
+					<li class="services-list_item-apps_thing">Система управления</li>
+					<li class="services-list_item-apps_thing">Более 10 страниц</li>
+					<li class="services-list_item-apps_thing">SEO оптимизация</li>
+				</ul>
+				<a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
+			</article>
+			<article class="services-list_item">
+				<h3 class="services-list_item-title">Промо-сайт</h3>
+				<img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="Landing Page LIS" class="services-list_item-example">
+				<p class="services-list_item-price">от <span>21 990</span> р.</p>
+				<ul class="services-list_item-apps">
+					<li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
+					<li class="services-list_item-apps_thing">Мобильная версия сайта</li>
+					<li class="services-list_item-apps_thing">Ориентация на праздник</li>
+					<li class="services-list_item-apps_thing">Fast-Маркетинг</li>
+					<li class="services-list_item-apps_thing">Контекстная реклама</li>
+				</ul>
+				<a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
+			</article>
+			<article class="services-list_item">
+				<h3 class="services-list_item-title">Сайт-визитка</h3>
+				<img src="<?=get_template_directory_uri();?>/assets/img/services/visit.png" alt="Landing Page LIS" class="services-list_item-example">
+				<p class="services-list_item-price">от <span>24 990</span> р.</p>
+				<ul class="services-list_item-apps">
+					<li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
+					<li class="services-list_item-apps_thing">CMS для работы с контентом</li>
+					<li class="services-list_item-apps_thing">Уникальный дизайн</li>
+					<li class="services-list_item-apps_thing">Многостраничный сайт</li>
+					<li class="services-list_item-apps_thing">SEO оптимизация</li>
+				</ul>
+				<a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
+			</article>
+		</div>
+	</div>
+</section>
