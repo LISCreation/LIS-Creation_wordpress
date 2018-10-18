5cce1e4d01b96d1933b3a4692d1a72a3d6b049b9
LIS Creation <lis.lightinsky@gmail.com>
2018-10-18 21:21:03 +0300

    Add menu place

--- wp-content/themes/lis-creation/functions.php
+++ wp-content/themes/lis-creation/functions.php
@@ -1,6 +1,7 @@
 <?php
 add_action('wp_enqueue_scripts', 'add_self_styles');
 add_action('wp_enqueue_scripts', 'add_self_scripts');
+add_action('after_setup_theme', 'register_main_nav_menu');
 
 function add_self_styles() {
 	wp_enqueue_style('bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
@@ -14,4 +15,7 @@ function add_self_scripts() {
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('owl-carousel', get_template_directory_uri().'/assets/js/owl.carousel.min.js', array(), null, true);
 	wp_enqueue_script('main', get_template_directory_uri().'/assets/js/main.min.js', array(), null, true);
+}
+function register_main_nav_menu() {
+	register_nav_menu('main', 'Main Location');
 }
