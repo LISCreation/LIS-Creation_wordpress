3a2543370d845000fa0faa981928742b68ea3279
LIS Creation <lis.lightinsky@gmail.com>
2018-10-19 18:31:11 +0300

    Add dynamic sidebar

--- wp-content/themes/lis-creation/index.php
+++ wp-content/themes/lis-creation/index.php
@@ -1,48 +1,6 @@
 <?php get_header().wp_head();?>
 <body class="clearfix">
-<div class="global-sidebar">
-    <!--spare_html: Mono Menu-->
-    <div class="global-sidebar_up">
-        <div class="global-sidebar_up_icon-wrapper">
-            <a href="#toTop" class="anchor"><img class="global-sidebar_icon global-sidebar_up_icon" src="<?=get_template_directory_uri();?>/assets/img/icons/up.svg" alt="Вверх"></a>
-        </div>
-    </div>
-    <div class="global-sidebar_middle">
-        <nav class="mono">
-            <ul class="mono-menu">
-                <li class="mono-menu_item">
-                    <a href="#toTop"><div class="tab">Вверх</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a href="#achievements"><div class="tab">О нас в цифрах</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a href="#works"><div class="tab">Лучшие работы</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a href="#team"><div class="tab">Наша команда</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a href="#services"><div class="tab">Наши услуги</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a href="#why"><div class="tab">Почему именно мы?</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a href="#callback"><div class="tab">Заказать звонок</div></a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    <div class="global-sidebar_down">
-        <div class="global-sidebar_down_icon-wrapper">
-            <a href="tel:79384067802"><img src="<?=get_template_directory_uri();?>/assets/img/icons/call.svg" alt="Позвонить!" class="global-sidebar_icon"></a>
-        </div>
-        <div class="global-sidebar_down_icon-wrapper">
-            <a href="mailto:lis.lightinsky@gmail.com"><img src="<?=get_template_directory_uri();?>/assets/img/icons/email.svg" alt="Позвонить!" class="global-sidebar_icon"></a>
-        </div>
-    </div>
-</div>
+<?php get_template_part('template-parts/block_sidebar', 'main');?>
 <div class="global-wrapper">
     <header class="header" id="header">
         <?php
