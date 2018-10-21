06b9d64ac5b799c74c6c642a5ebfd322312d7f87
LIS Creation <lis.lightinsky@gmail.com>
2018-10-21 20:25:29 +0300

    Add service.php & set ACF

--- wp-content/themes/lis-creation/page-templates/service.php
+++ wp-content/themes/lis-creation/page-templates/service.php
@@ -5,47 +5,12 @@
 ?>
 <?php get_header().wp_head();?>
 <body class="clearfix service">
-<div class="global-sidebar  global-sidebar_service__landing">
-    <!--spare_html: Mono Menu-->
-    <div class="global-sidebar_up">
-        <div class="global-sidebar_up_icon-wrapper">
-            <img class="global-sidebar_icon global-sidebar_up_icon" src="<?=get_template_directory_uri();?>/assets/img/icons/up.svg" alt="Вверх">
-        </div>
-    </div>
-    <div class="global-sidebar_middle">
-        <nav class="mono">
-            <ul class="mono-menu">
-                <li class="mono-menu_item">
-                    <a class="anchor" href="#"><div class="tab">Начало экрана</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a class="anchor" href="#"><div class="tab">Начало экрана 2</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a class="anchor" href="#"><div class="tab">Начало экрана Начало экрана</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a class="anchor" href="#"><div class="tab">Начало экрана Начало экранаНачало экрана</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a class="anchor" href="#"><div class="tab">Начало экрана Начало экрана</div></a>
-                </li>
-                <li class="mono-menu_item">
-                    <a class="anchor" href="#"><div class="tab">Начало экрана 32</div></a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    <div class="global-sidebar_down">
-        <div class="global-sidebar_down_icon-wrapper">
-            <img src="<?=get_template_directory_uri();?>/assets/img/icons/call.svg" alt="Позвонить!" class="global-sidebar_icon">
-        </div>
-        <div class="global-sidebar_down_icon-wrapper">
-            <img src="<?=get_template_directory_uri();?>/assets/img/icons/email.svg" alt="Позвонить!" class="global-sidebar_icon">
-        </div>
-    </div>
-</div>
-<div class="global-wrapper serv">
+<?php get_template_part('template-parts/block_sidebar', 'services')?>
+<div class="global-wrapper serv <?php
+    $serv_id = get_field('service_id-name');
+    if($serv_id != 'landing') echo 'another ';
+    the_field('service_id-name');
+?>">
     <header class="header serv-header" id="header">
         <div class="container-self">
 	        <?php
@@ -68,10 +33,10 @@
                         <div class="info">
 	                        <?php the_content();?>
                         </div>
-                        <div class="btn_roundness btn_roundness-reverse serv-header-button"><a class="works-link" href="#"><div class="btn btn-reverse">Узнать больше!</div></a></div>
+                        <div class="btn_roundness btn_roundness-reverse serv-header-button"><a class="works-link" href="#callback"><div class="btn btn-reverse">Узнать больше!</div></a></div>
                     </div>
                     <div class="serv-header-image_wrapper">
-                        <img src="<?=get_template_directory_uri();?>/assets/img/services/landing.png" class="serv-header-image" alt="Landing page одностраничный">
+                        <img src="<?php the_field('main_image');?>" class="serv-header-image" alt="Landing page одностраничный">
                     </div>
                 </div>
             </div>
@@ -80,65 +45,55 @@
     <section class="listing tasks" id="tasks">
         <div class="container">
             <div class="section-info">
-                <h2 class="section-header">Задачи <?php the_title();?></h2>
-                <p class="section-description"><?php the_title();?> представляет посетителю уникальное товарное
-                    предложение, и содержит в себе максимум информации при минимуме
-                    объема. Ее цель – привлечь и задержать посетителя, подталкивая
-                    его к тому, чтобы стать Вашим клиентом.</p>
+                <h2 class="section-header">Задачи <?php the_field('rod_name');?></h2>
+                <p class="section-description"><?php the_field('tasks_desc');?></p>
             </div>
             <div class="listing-list tasks-list">
                 <div class="row">
                     <div class="col-lg-3 col-sm-6">
                         <div class="listing-list_item tasks-list_item">
-                            <div class="listing-list_item-icon tasks-list_item-icon"><img src="<?=get_template_directory_uri();?>/assets/img/icons/services/startup.svg" alt="Rocket LIS"></div>
-                            <p class="listing-list_item-designation tasks-list_item-designation">Призыв к активному действию. Это может быть регистрация или покупка</p>
+                            <div class="listing-list_item-icon tasks-list_item-icon"><img src="<?php the_field('task_image1')?>" alt="Rocket LIS"></div>
+                            <p class="listing-list_item-designation tasks-list_item-designation"><?php the_field('task1');?></p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="listing-list_item tasks-list_item">
-                            <div class="listing-list_item-icon tasks-list_item-icon"><img src="<?=get_template_directory_uri();?>/assets/img/icons/services/approve.svg" alt="Rocket LIS"></div>
-                            <p class="listing-list_item-designation tasks-list_item-designation">Максимум информативности при минимальной текстовой загрузке</p>
+                            <div class="listing-list_item-icon tasks-list_item-icon"><img src="<?php the_field('task_image2')?>" alt="<?=basename(get_field(''))?>"></div>
+                            <p class="listing-list_item-designation tasks-list_item-designation"><?php the_field('task2');?></p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="listing-list_item tasks-list_item">
-                            <div class="listing-list_item-icon tasks-list_item-icon"><img src="<?=get_template_directory_uri();?>/assets/img/icons/services/responsive.svg" alt="Rocket LIS"></div>
-                            <p class="listing-list_item-designation tasks-list_item-designation">Доступность страницы для пользователя на любых устройствах</p>
+                            <div class="listing-list_item-icon tasks-list_item-icon"><img src="<?php the_field('task_image3')?>" alt="Rocket LIS"></div>
+                            <p class="listing-list_item-designation tasks-list_item-designation"><?php the_field('task3');?></p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="listing-list_item tasks-list_item">
-                            <div class="listing-list_item-icon tasks-list_item-icon"><img src="<?=get_template_directory_uri();?>/assets/img/icons/services/competition.svg" alt="Rocket LIS"></div>
-                            <p class="listing-list_item-designation tasks-list_item-designation">Привлечь внимание и превратить посетителя сайта в покупателя</p>
+                            <div class="listing-list_item-icon tasks-list_item-icon"><img src="<?php the_field('task_image4')?>" alt="Rocket LIS"></div>
+                            <p class="listing-list_item-designation tasks-list_item-designation"><?php the_field('task4');?></p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <section class="process">
+    <section class="process" id="process">
         <div class="container">
             <div class="section-info">
-                <h2 class="section-header">создание landing page</h2>
-                <p class="section-description">Когда требуется обратить внимание клиента на ваш определенный товар
-                    или услугу, то значит, вам необходим Landing - продающая страница!
-                    Наша команда профессионалов предлагает интересные решения
-                    для эффективной продажи и продвижения
-                    вашего товара или услуги. </p>
+                <h2 class="section-header">создание <?php the_field('rod_name');?></h2>
+                <p class="section-description"><?php the_field('process_desc');?></p>
             </div>
             <div class="process-info">
-                <p class="process-text"><strong>Команда LIS Creation</strong> разработает для Вас продающий
-                    <?php the_title();?> с учетом специфики товара/услуги,
-                    а так же анализом рынка и конкуренов.
-                </p>
+                <p class="process-text"><?php the_field('process_team');?></p>
                 <div class="process-numbers">
-                    <p class="process-price">Срок разработки проекта - <span>14-21 дней</span></p>
-                    <p class="process-duration">Стоимость проекта - <span>от 21 990 руб.</span></p>
+                    <p class="process-price">Срок разработки проекта - <strong><?php the_field('process_time');?></strong></p>
+                    <p class="process-duration">Стоимость проекта - <strong>от <?php the_field('process_price')?> руб.</strong></p>
                 </div>
                 <div class="process-button"><a class="works-link" href="#"><div class="btn_roundness"><div class="btn btn-reverse process_press">Заказать</div></div></a></div>
             </div>
             <div class="process-image_wrapper">
-                <img src="<?=get_template_directory_uri();?>/assets/img/services/landings.png" alt="3d example from works" class="process-image">
+                <img src="<?php the_field('process_image')?>" alt="3d example from works" class="process-image">
             </div>
         </div>
     </section>
@@ -146,7 +101,8 @@
         <div class="container">
             <div class="section-info">
                 <h2 class="section-header">Что мы  предлагаем?</h2>
-                <p class="section-description">Для Вашего удобства мы разработали систему «под ключ» - от вашей
+                <p class="section-description">
+                    Для Вашего удобства мы разработали систему «под ключ» - от вашей
                     идеи до продвижения на передовые позиции в интернете.
                     Мы работаем только на результат!</p>
             </div>
@@ -180,123 +136,8 @@
             </div>
         </div>
     </section>
-    <section class="services" id="services">
-        <div class="container-fluid p-0">
-            <div class="section-info">
-                <h2 class="section-header">наши услуги</h2>
-                <p class="section-description">За 4 года более восьмидесяти клиентов стали довольными
-                    обладателями собственных страниц в интернете. Наша задача,
-                    чтобы посетители страницы стали вашими покупателями.</p>
-            </div>
-            <div class="services-list owl-carousel">
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title"><?php the_title();?></h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/landing.png" alt="<?php the_title();?> by LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>21 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">Мобильная версия сайта</li>
-                        <li class="services-list_item-apps_thing">Уникальный дизайн</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация</li>
-                        <li class="services-list_item-apps_thing">Размещение на хостинге</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Интернет-магазин</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/ecommerce-shop.png" alt="<?php the_title();?> LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>32 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">Мобильная версия сайта</li>
-                        <li class="services-list_item-apps_thing">Уникальный дизайн</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация</li>
-                        <li class="services-list_item-apps_thing">CMS на выбор **</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Блог / Информационный сайт</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="<?php the_title();?> LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>27 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">CMS для работы с контентом</li>
-                        <li class="services-list_item-apps_thing">Многостраничный сайт</li>
-                        <li class="services-list_item-apps_thing">Мобильная оптимизация</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация (x2)</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Корпоративный сайт</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="<?php the_title();?> LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>29 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">Мобильная версия сайта</li>
-                        <li class="services-list_item-apps_thing">Система управления</li>
-                        <li class="services-list_item-apps_thing">Более 10 страниц</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Промо-сайт</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/example.png" alt="<?php the_title();?> LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>21 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">Мобильная версия сайта</li>
-                        <li class="services-list_item-apps_thing">Ориентация на праздник</li>
-                        <li class="services-list_item-apps_thing">Fast-Маркетинг</li>
-                        <li class="services-list_item-apps_thing">Контекстная реклама</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-                <article class="services-list_item">
-                    <h3 class="services-list_item-title">Сайт-визитка</h3>
-                    <img src="<?=get_template_directory_uri();?>/assets/img/services/visit.png" alt="<?php the_title();?> LIS" class="services-list_item-example">
-                    <p class="services-list_item-price">от <span>24 990</span> р.</p>
-                    <ul class="services-list_item-apps">
-                        <li class="services-list_item-apps_thing">Адаптивная верстка HTML5/CSS3</li>
-                        <li class="services-list_item-apps_thing">CMS для работы с контентом</li>
-                        <li class="services-list_item-apps_thing">Уникальный дизайн</li>
-                        <li class="services-list_item-apps_thing">Многостраничный сайт</li>
-                        <li class="services-list_item-apps_thing">SEO оптимизация</li>
-                    </ul>
-                    <a class="works-link" href="#"><div class="btn_roundness btn_roundness-reverse"><div class="btn btn-reverse services-button">Хочу такой</div></div></a>
-                </article>
-            </div>
-        </div>
-    </section>
-    <section class="callback" id="callback">
-        <div class="container">
-            <div class="section-info">
-                <h2 class="section-header">свяжитесь с нами</h2>
-                <p class="section-description">Заполните форму и мы с Вами свяжемся
-                    в ближайшее время!</p>
-            </div>
-            <form action="javascript:void(null)" id="callback_main" method="post" class="callback-form">
-                <div class="callback-form_inner">
-                    <div class="inp_wrapper">
-                        <input type="text" name="name" placeholder="Ваше имя" class="inp callback-form-input">
-                        <span class="inp_starlet">*</span>
-                    </div>
-                    <div class="inp_wrapper">
-                        <input type="tel" name="phone" placeholder="Введите номер" class="inp callback-form-input phone_inp">
-                        <span class="inp_starlet">*</span>
-                    </div>
-                    <div class="inp_wrapper">
-                        <input type="email" name="email" placeholder="Ваш E-mail" class="inp callback-form-input">
-                        <span class="inp_starlet">*</span>
-                    </div>
-                    <div class="callback-form-button_wrapper">
-                        <div class="btn_roundness"><input type="submit" value="Оставить заявку" class="btn callback-form-button"></div>
-                    </div>
-                </div>
-                <div id="callback_main_result"></div>
-            </form>
-        </div>
-    </section>
-<?php wp_footer().get_footer();?>
+<?php
+get_template_part('template-parts/section_service', 'main');
+get_template_part('template-parts/section_callback', 'main');
+wp_footer().get_footer();
+?>
---
+++ wp-content/themes/lis-creation/template-parts/block_sidebar-services.php
@@ -0,0 +1,39 @@
+<div class="global-sidebar">
+	<!--spare_html: Mono Menu-->
+	<div class="global-sidebar_up">
+		<div class="global-sidebar_up_icon-wrapper">
+			<a href="#toTop" class="anchor"><img class="global-sidebar_icon global-sidebar_up_icon" src="<?=get_template_directory_uri();?>/assets/img/icons/up.svg" alt="Вверх"></a>
+		</div>
+	</div>
+	<div class="global-sidebar_middle">
+		<nav class="mono">
+			<ul class="mono-menu">
+				<li class="mono-menu_item">
+					<a href="#toTop"><div class="tab">Вверх</div></a>
+				</li>
+                <li class="mono-menu_item">
+                    <a href="#tasks"><div class="tab">Задачи</div></a>
+                </li>
+				<li class="mono-menu_item">
+					<a href="#process"><div class="tab">Создание</div></a>
+				</li>
+				<li class="mono-menu_item">
+					<a href="#offer"><div class="tab">Что мы предлагаем?</div></a>
+				</li>
+                <li class="mono-menu_item">
+                    <a href="#services"><div class="tab">Наши услуги</div></a>
+                </li>
+				<li class="mono-menu_item">
+					<a href="#callback"><div class="tab">В конец</div></a>
+			</ul>
+		</nav>
+	</div>
+	<div class="global-sidebar_down">
+		<div class="global-sidebar_down_icon-wrapper">
+			<a href="tel:79384067802"><img src="<?=get_template_directory_uri();?>/assets/img/icons/call.svg" alt="Позвонить!" class="global-sidebar_icon"></a>
+		</div>
+		<div class="global-sidebar_down_icon-wrapper">
+			<a href="mailto:lis.lightinsky@gmail.com"><img src="<?=get_template_directory_uri();?>/assets/img/icons/email.svg" alt="Позвонить!" class="global-sidebar_icon"></a>
+		</div>
+	</div>
+</div>
