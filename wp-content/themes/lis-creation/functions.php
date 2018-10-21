99c2f2e5fb79716c25c3718b3201967b5b1db681
LIS Creation <lis.lightinsky@gmail.com>
2018-10-21 11:17:38 +0300

    Add taxonomy

--- wp-content/themes/lis-creation/functions.php
+++ wp-content/themes/lis-creation/functions.php
@@ -37,12 +37,51 @@ function register_custom_ptypes() {
 		'menu_name' => 'Портфолио' // ссылка в меню в админке
 	);
 	$args = array(
+		'rewrite' => array('slug' => 'works'),
 		'labels' => $labels,
 		'public' => true, // благодаря этому некоторые параметры можно пропустить
 		'menu_icon' => 'dashicons-welcome-view-site', // иконка корзины
 		'menu_position' => 5,
+		'taxonomies' => array('works'),
 		'has_archive' => true,
 		'supports' => array( 'title', 'editor', 'thumbnail')
 	);
-	register_post_type('works',$args);
+	register_post_type('work', $args);
+}
+add_action('init', 'create_taxonomy');
+function create_taxonomy(){
+	// список параметров: http://wp-kama.ru/function/get_taxonomy_labels
+	register_taxonomy('pages', array('work'), array(
+		'label'                 => '', // определяется параметром $labels->name
+		'labels'                => array(
+			'name'              => 'Pages',
+			'singular_name'     => 'Страница',
+			'search_items'      => 'Искать страницы',
+			'all_items'         => 'Все страницы',
+			'view_item '        => 'Смотреть страницу',
+			'edit_item'         => 'Редактировать страницу',
+			'update_item'       => 'Обновить страницу',
+			'add_new_item'      => 'Добавить страницу',
+			'new_item_name'     => 'Имя новой страницы',
+			'menu_name'         => 'Страницы',
+		),
+		'description'           => '', // описание таксономии
+		'public'                => true,
+		'publicly_queryable'    => null, // равен аргументу public
+		'show_in_nav_menus'     => true, // равен аргументу public
+		'show_ui'               => true, // равен аргументу public
+		'show_in_menu'          => true, // равен аргументу show_ui
+		'show_tagcloud'         => true, // равен аргументу show_ui
+		'show_in_rest'          => null, // добавить в REST API
+		'rest_base'             => null, // $taxonomy
+		'hierarchical'          => true,
+		'update_count_callback' => '',
+		'rewrite'               => array('slug' => null),
+		//'query_var'             => $taxonomy, // название параметра запроса
+		'capabilities'          => array(),
+		'meta_box_cb'           => null, // callback функция. Отвечает за html код метабокса (с версии 3.8): post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
+		'show_admin_column'     => false, // Позволить или нет авто-создание колонки таксономии в таблице ассоциированного типа записи. (с версии 3.5)
+		'_builtin'              => false,
+		'show_in_quick_edit'    => null, // по умолчанию значение show_ui
+	) );
 }
