30f265794d21b1c9c5dcc4765717f80c087f66ac
LIS Creation <lis.lightinsky@gmail.com>
2018-10-21 11:19:55 +0300

    Add templates for works & fix links

---
+++ wp-content/themes/lis-creation/single-work.php
@@ -0,0 +1,95 @@
+<?php get_header().wp_head();?>
+<body class="work clearfix">
+<div class="global-sidebar global-sidebar_work">
+	<div class="global-sidebar_up">
+		<div class="global-sidebar_up_icon-wrapper">
+			<a href="<?=get_term_link(7, 'pages');?>"><img class="global-sidebar_icon global-sidebar_up_icon" src="<?=get_template_directory_uri();?>/assets/img/icons/up.svg" alt="Вверх"></a>
+		</div>
+	</div>
+	<div class="global-sidebar_down">
+		<div class="global-sidebar_down_icon-wrapper">
+			<img src="<?=get_template_directory_uri();?>/assets/img/icons/call.svg" alt="Позвонить!" class="global-sidebar_icon">
+		</div>
+		<div class="global-sidebar_down_icon-wrapper">
+			<img src="<?=get_template_directory_uri();?>/assets/img/icons/email.svg" alt="Позвонить!" class="global-sidebar_icon">
+		</div>
+	</div>
+</div>
+<div class="global-wrapper work-wrapper">
+	<header class="work-header">
+		<div class="work-header-init_wrapper">
+			<div class="work-header-init">
+				<h5 class="work-header-init_service">Разработка сайта под ключ</h5>
+				<h1 class="work-header-init_title">коттеджный поселок “лесной пейзаж”</h1>
+			</div>
+		</div>
+		<div class="work-header-init__list">
+			<div class="work-header-init_link"><a href="https://advocat-krd.ru" rel="nofollow" target="_blank">https://advocat-krd.ru</a></div>
+			<div class="work-header-init_want">
+				<div class="btn_roundness btn_roundness-reverse"><div class="btn works-link-button work-header-init_click">Хочу такой же!</div></div>
+			</div>
+			<div class="work-header-init_type">
+				<p>Тип сайта: <a href="#">Landing Page</a></p>
+			</div>
+		</div>
+		<div class="work-header-slider">
+			<div class="work-header-slider_area owl-carousel">
+				<div class="work-header-slider_item">
+					<img src="https://klubputeshestvennikov.com/wp-content/uploads/2017/01/Los-Anzheles-9.jpg" class="back" alt="LIS creation work">
+					<img src="https://klubputeshestvennikov.com/wp-content/uploads/2017/01/Los-Anzheles-9.jpg" class="front" alt="LIS creation work">
+				</div>
+				<div class="work-header-slider_item">
+					<img src="<?=get_template_directory_uri();?>/assets/img/les.jpg" class="back" alt="LIS creation work">
+					<img src="<?=get_template_directory_uri();?>/assets/img/les.jpg" class="front" alt="LIS creation work">
+				</div>
+				<div class="work-header-slider_item">
+					<img src="https://cs9.pikabu.ru/post_<?=get_template_directory_uri();?>/assets/img/2017/08/08/7/og_og_1502191127255817837.jpg" class="back" alt="LIS creation work">
+					<img src="https://cs9.pikabu.ru/post_<?=get_template_directory_uri();?>/assets/img/2017/08/08/7/og_og_1502191127255817837.jpg" class="front" alt="LIS creation work">
+				</div>
+				<div class="work-header-slider_item">
+					<img src="http://ftp.fisti.ru/sites/default/files/files/images/fisti_ru_22489.jpg" class="back" alt="LIS creation work">
+					<img src="http://ftp.fisti.ru/sites/default/files/files/images/fisti_ru_22489.jpg" class="front" alt="LIS creation work">
+				</div>
+				<div class="work-header-slider_item">
+					<img src="<?=get_template_directory_uri();?>/assets/img/les.jpg" class="back" alt="LIS creation work">
+					<img src="<?=get_template_directory_uri();?>/assets/img/les.jpg" class="front" alt="LIS creation work">
+				</div>
+			</div>
+		</div>
+		<div class="work-header-list_wrapper">
+			<button class="work-header_more"><img src="<?=get_template_directory_uri();?>/assets/img/icons/arrow-down.svg" alt=""><p>Что мы сделали?</p></button>
+			<ul class="work-header-list">
+				<li class="work-header-list_item">Создание ТЗ, анализ рынка и конкурентов</li>
+				<li class="work-header-list_item">Разработка структуры сайта, создание детального прототипа</li>
+				<li class="work-header-list_item">Разрботка и создание уникального дизайна сайта</li>
+				<li class="work-header-list_item">Адаптивная и кроссбраузерная верстка сайта</li>
+				<li class="work-header-list_item">Установка на хостинг</li>
+				<li class="work-header-list_item">SEO оптимизация</li>
+
+				<li class="work-header-list_item">Создание ТЗ, анализ рынка и конкурентов</li>
+				<li class="work-header-list_item">Разработка структуры сайта, создание детального прототипа</li>
+				<li class="work-header-list_item">Разрботка и создание уникального дизайна сайта</li>
+				<li class="work-header-list_item">Адаптивная и кроссбраузерная верстка сайта</li>
+				<li class="work-header-list_item">Установка на хостинг</li>
+				<li class="work-header-list_item">SEO оптимизация</li>
+				<li class="work-header-list_item">Создание ТЗ, анализ рынка и конкурентов</li>
+				<li class="work-header-list_item">Разработка структуры сайта, создание детального прототипа</li>
+				<li class="work-header-list_item">Разрботка и создание уникального дизайна сайта</li>
+				<li class="work-header-list_item">Адаптивная и кроссбраузерная верстка сайта</li>
+				<li class="work-header-list_item">Установка на хостинг</li>
+				<li class="work-header-list_item">SEO оптимизация</li>
+
+				<li class="work-header-list_item">Создание ТЗ, анализ рынка и конкурентов</li>
+				<li class="work-header-list_item">Разработка структуры сайта, создание детального прототипа</li>
+				<li class="work-header-list_item">Разрботка и создание уникального дизайна сайта</li>
+				<li class="work-header-list_item">Адаптивная и кроссбраузерная верстка сайта</li>
+				<li class="work-header-list_item">Установка на хостинг</li>
+				<li class="work-header-list_item">SEO оптимизация</li>
+
+			</ul>
+		</div>
+	</header>
+</div>
+<?php wp_footer();?>
+</body>
+</html>
